d6069741c1c422161df957c7441cc7b5892bcdd3
titiyocretton <titiyo.cretton@studentfr.ch>
2024-04-17 09:31:50 +0200

    faire montrer et cacher exercice et couleur bouton dedan

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,10 @@ var change = document.querySelector(".button-radio");
 var optionImpression = document.querySelector(".cta-option-dropdown");
 var hiddenOptionImpression = document.querySelector(".hidden-option");
 
+var startExerce = document.querySelector(".button-exerce");
+var exercice = document.querySelector(".lightbox");
+var crossExercice = document.querySelector(".exercice-cross");
+
 var clickRight = function () {
   left.classList.remove("active");
   right.classList.add("active");
@@ -23,3 +27,15 @@ right.addEventListener("click", clickRight);
 left.addEventListener("click", clickLeft);
 
 optionImpression.addEventListener("click", impression);
+
+var exerceVisible = function () {
+  exercice.classList.remove("lightbox-hidden");
+};
+
+startExerce.addEventListener("click", exerceVisible);
+
+var exerceHidden = function () {
+  exercice.classList.add("lightbox-hidden");
+};
+
+crossExercice.addEventListener("click", exerceHidden);
